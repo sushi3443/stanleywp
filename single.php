fe2ffacfe6f46f6ee85a3fe07831fc30bee1eeb3
Joleen Fetter <joleenfetter@gmail.com>
2017-12-18 13:41:03 -0500

    single blog layout

--- single.php
+++ single.php
@@ -9,23 +9,27 @@
 
 get_header(); ?>
 
-	<div class="container">
-		<div class="row">
-			<div id="primary" class="content-area">
+			<div id="primary" class="content-area-full">
 				<main id="main" class="site-main" role="main">
 
 				<?php
 				while ( have_posts() ) : the_post();
 
-					get_template_part( 'template-parts/content', get_post_format() );
+					get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-					the_post_navigation();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-md-8">
+								<?php the_post_navigation(); 
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) :
 						comments_template();
-					endif;
+					endif; ?>
+							</div>
+						</div>
+					</div>
 
+				<?php	
 				endwhile; // End of the loop.
 				?>
 
@@ -33,5 +37,4 @@ get_header(); ?>
 			</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
