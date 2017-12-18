76d89490ece834615b837b763f82966a840da193
Jeremy Fetter <iShishi@Joleens-iMac.local>
2017-12-18 11:48:01 -0500

    start header and footer

--- footer.php
+++ footer.php
@@ -15,6 +15,17 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<?php dynamic_sidebar( 'footer-1' ); ?>
+				</div>
+				<div class="col-md-4">
+					<?php dynamic_sidebar( 'footer-2' ); ?>
+				</div>
+				<div class="col-md-4">
+					<?php dynamic_sidebar( 'footer-3' ); ?>
+				</div>
+			</div>
 			<div class="site-info">
 				&copy; <?php bloginfo( 'name' );
 						echo ' - ';
